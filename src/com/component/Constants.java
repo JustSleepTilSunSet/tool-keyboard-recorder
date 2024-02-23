fda2cd3f55df3a91ed7210075b8c2b72a9b1fe51
JustSleepTilSunSet <cloverchen.042@gmail.com>
2024-02-23 21:56:24 +0800

    Revise constants.

--- src/com/component/Constants.java
+++ src/com/component/Constants.java
@@ -2,4 +2,8 @@ package com.component;
 import java.awt.Font;
 public class Constants{
     public static Font Arial_Bold_16 = new Font("Arial Bold",Font.BOLD,16);
+    public static int SPINNER_WIDTH = 50;
+    public static int SPINNER_HEIGHT = 50;
+    public static int CONFIRM_BTN_WIDTH = 100;
+    public static int CONFIRM_BTN_HEIGHT = 50;
 }
--- src/com/main/CtlFrame.java
+++ src/com/main/CtlFrame.java
@@ -2,6 +2,7 @@ package com.main;
 import com.component.ConfirmBtn;
 import com.component.ConfirmBtnActions;
 import com.component.FrameTextView;
+import com.component.Constants;
 
 import javax.swing.JSpinner;
 import javax.swing.JFrame;
@@ -15,21 +16,26 @@ import java.awt.Dimension;
 class CtlFrame extends JFrame{
     JSpinner spinner = new JSpinner();
     JButton btn = new JButton("Start");
+    int SPINNER_WIDTH = Constants.SPINNER_WIDTH;
+    int SPINNER_HEIGHT = Constants.SPINNER_HEIGHT;
+    int CONFIRM_BTN_WIDTH = Constants.CONFIRM_BTN_WIDTH;
+    int CONFIRM_BTN_HEIGHT = Constants.CONFIRM_BTN_HEIGHT;
+
     public CtlFrame(String title){
         super(title);
         this.setLayout(new GridLayout(1,5));
         this.setLocation(100,200);
         this.setSize(850,100);
         this.setResizable(false);
-        spinner.setMaximumSize(new Dimension(50,50));
-        btn.setMaximumSize(new Dimension(100,50));
+        spinner.setMaximumSize(new Dimension(SPINNER_WIDTH, SPINNER_HEIGHT));
+        btn.setMaximumSize(new Dimension(CONFIRM_BTN_WIDTH, CONFIRM_BTN_WIDTH));
         this.add(new FrameTextView("Delay record time:"));
         this.add(spinner); // Set delay time.
-        ConfirmBtn confirmBtn = new ConfirmBtn("Start");
-        this.add(confirmBtn.setAction(new ConfirmBtnActions().setBindSpinner(spinner)));
         this.add(new JLabel("TBD1"));
         this.add(new JLabel("TBD2"));
         this.add(new JLabel("TBD3"));
+        ConfirmBtn confirmBtn = new ConfirmBtn("Start");
+        this.add(confirmBtn.setAction(new ConfirmBtnActions().setBindSpinner(spinner)));
         this.setVisible(true);
     }
 }
